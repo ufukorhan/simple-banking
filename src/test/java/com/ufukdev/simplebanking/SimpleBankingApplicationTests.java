21fae01c2c858d523a22d97d68901c1b7289cf9e
Ufuk ORHAN <91383831+ufukorhan@users.noreply.github.com>
2024-02-23 19:52:28 +0300

    S-BANKING-1: Init simple-banking-api

---
+++ src/test/java/com/ufukdev/simplebanking/SimpleBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufukdev.simplebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
