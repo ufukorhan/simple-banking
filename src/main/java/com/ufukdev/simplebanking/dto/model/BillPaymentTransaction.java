5e66bd0c5167667bd372097c34e32af7116454e0
Ufuk ORHAN <91383831+ufukorhan@users.noreply.github.com>
2024-02-26 10:13:48 +0300

    S-BANKING-8: Create BillPaymentTransaction

---
+++ src/main/java/com/ufukdev/simplebanking/dto/model/BillPaymentTransaction.java
@@ -0,0 +1,16 @@
+package com.ufukdev.simplebanking.dto.model;
+
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+public class BillPaymentTransaction extends Transaction {
+
+    private String payee;
+}
